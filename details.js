1b1da24404ee31f32e56f43fd9d36f2c8c730c8a
KavyaCOder1 <97831532+KavyaCOder1@users.noreply.github.com>
2024-11-18 22:23:16 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '5836906160:AAGbdNUBbXHjf7bmzAZaQA5H0NX2FYnqmuM',
+    token: '5836906160:',
     admins: [5556717138],
     mongo_url: 'mongodb+srv://:R4gO0q3MFPDbjqKp@kavyatg.zgsh5nz.mongodb.net/?retryWrites=true&w=majority',
     medialink: 'https://graph.org/file/aca9854398236a03783ec.jpg'
